a599035cd7a3fe531f907c9a4b4a06da8b103031
m_messiah <m.muzafarov@gmail.com>
2016-02-19 15:40:44 +0300

    environ: users.db and simple registration

--- services/environ/controller.py
+++ services/environ/controller.py
@@ -1,10 +1,39 @@
 from datetime import datetime
-
-from flask import Flask, request, render_template, session, url_for, redirect, flash
+from hashlib import sha256
+from flask import (Flask, request, render_template,
+                   session, url_for, redirect, flash)
 import os
-app = Flask("environ")
-app.secret_key = os.urandom(32)
-app.logs = os.path.dirname(os.path.realpath(__file__)) + "/logs/"
+
+
+class Users(object):
+    def __init__(self, filename):
+        self.filename = filename
+        try:
+            self._users = {
+                l.split(':')[1]: dict(zip(['uid', 'username', 'password'],
+                                          l.strip().split(':')))
+                for l in open(self.filename, 'r').readlines()
+            }
+        except FileNotFoundError:
+            self._users = {}
+            open(self.filename, 'w').write("")
+
+    def auth(self, username, password):
+        if username not in self._users:
+            raise LookupError
+        user = self._users[username]
+        if user['password'] != sha256(password.encode("utf8")).hexdigest():
+            raise KeyError
+        return user['uid'], user['username']
+
+    def __setitem__(self, key, value):
+        if key in self._users or ':' in key:
+            raise KeyError
+        user = {'uid': hash(key), 'username': key,
+                'password': sha256(value.encode("utf8")).hexdigest()}
+        with open(self.filename, 'a') as db:
+            self._users[key] = user
+            db.write("%(uid)s:%(username)s:%(password)s\n" % user)
 
 
 def tail(filename, n=1):
@@ -17,7 +46,7 @@ def tail(filename, n=1):
         return "\n".join(list(map(lambda l: l.decode("utf8").rstrip(),
                                   f.readlines()[-n:]))[::-1])
     except:
-        return b""
+        return ""
 
 
 def decode(raw, s_type):
@@ -31,6 +60,11 @@ def decode(raw, s_type):
         return raw
     else:
         return hash(raw) % 2 if raw else raw
+
+app = Flask("environ")
+app.secret_key = os.urandom(32)
+app.logs = os.path.dirname(os.path.realpath(__file__)) + "/logs/"
+app.users = Users(os.path.dirname(os.path.realpath(__file__)) + "/users.db")
 
 
 @app.route("/")
@@ -55,25 +89,35 @@ def dashboard():
                            sensors={k: decode(tail(k), k) for k in sensors})
 
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
-    return "Registration form here"
+    errors = {}
+    if request.method == 'POST':
+        try:
+            data = request.form
+            app.users[data.get('username', "")] = data.get('password', "")
+            flash('Registration successful! Please log in.')
+            return redirect(url_for('login'))
+        except (KeyError, AttributeError):
+            errors['username'] = True
+    return render_template("register.html", errors=errors)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     errors = {}
     if request.method == 'POST':
-        if request.form.get('username') == 'admin':
-            if request.form.get('password') == 'qwerty':
-                session['uid'] = 1
-                session['username'] = request.form.get('username')
-                session['logged_in'] = datetime.now().isoformat(sep=' ')
-                flash('Welcome home, sweet!')
-                return redirect(url_for('dashboard'))
-            else:
-                errors['password'] = True
-        else:
+        try:
+            session['uid'], session['username'] = app.users.auth(
+                request.form.get('username', ""),
+                request.form.get('password', "")
+            )
+            session['logged_in'] = datetime.now().isoformat(sep=' ')
+            flash('Welcome home, sweet!')
+            return redirect(url_for('dashboard'))
+        except KeyError:
+            errors['password'] = True
+        except LookupError:
             errors['username'] = True
     return render_template("login.html", errors=errors)
 
@@ -92,6 +136,7 @@ def log(sensor):
     if 'username' in session:
         return render_template("log.html", log=tail(sensor, 50), sensor=sensor)
     return redirect(url_for('dashboard'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- services/environ/tests/test_controller.py
+++ services/environ/tests/test_controller.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from unittest import TestCase
+from os import remove
 import controller
 __author__ = 'm_messiah'
 
@@ -8,6 +9,12 @@ class TestController(TestCase):
     def setUp(self):
         controller.app.config['TESTING'] = True
         self.app = controller.app.test_client(use_cookies=True)
+        self.app.application.users = controller.Users("tests/test_users.db")
+        self.app.application.users["admin"] = "qwerty"
+
+    def tearDown(self):
+        del self.app.application.users._users["admin"]
+        remove("tests/test_users.db")
 
     def test_dashboard(self):
         resp = self.app.get("/")
@@ -15,6 +22,10 @@ class TestController(TestCase):
 
     def test_register(self):
         self.assertIn(b"Registration", self.app.get("/register").data)
+        post = self.app.post("/register", data={'username': "admin1",
+                                                'password': "qwerty1"},
+                             follow_redirects=True).data
+        self.assertIn(b"Registration successful! Please log in.", post)
 
     def test_login(self):
         self.assertIn(b"Login", self.app.get("/login").data)
@@ -26,7 +37,8 @@ class TestController(TestCase):
 
     def test_log(self):
         self.app.post("/login", data={'username': "admin",
-                                      'password': "qwerty"})
+                                      'password': "qwerty"},
+                      follow_redirects=True)
         self.assertIn(b"Log for temperature",
                       self.app.get("/log/temperature").data)
 
