fdbc450690452ad441aed424567b564cee773d1d
m_messiah <m.muzafarov@gmail.com>
2016-03-17 13:09:40 +0300

    🏡 environ: да будет wifi!

--- services/environ/checker/checker.py
+++ services/environ/checker/checker.py
@@ -2,13 +2,68 @@ from base64 import b64encode
 from os import urandom
 from random import choice, randint
 from sys import argv, stderr
-from socket import error as net_error, socket
+from socket import error as net_error
+
+import requests
+from scapy.all import *
+from scapy.layers.dot11 import RadioTap, Dot11
+from scapy.layers.l2 import LLC
 
 __author__ = 'm_messiah'
 
 OK, GET_ERROR, CORRUPT, FAIL, INTERNAL_ERROR = 101, 102, 103, 104, 110
 PORT = 27000
-SENSORS = ("main", "door_main", "window_kitchen")
+
+
+class Env(Packet):
+    fields_desc = [
+        IntField('decoded', 31337),
+        IntField("team_id", 0),
+        FieldLenField("len", None, length_of="data"),
+        StrLenField("data", "", length_from=lambda pkt: pkt.len),
+        LongField("sign", 0),
+    ]
+
+bind_layers(LLC, Env, ssap=0)
+
+
+def send_package(team_id, data, secret=0):
+    encoded_data = int.from_bytes(data.encode("utf8"), byteorder='big')
+    if secret:
+        encoded_data *= secret
+    sign = 2707
+    pkg = RadioTap() / Dot11(type=2) / LLC() / Env(
+        team_id=team_id,
+        data=str(encoded_data),
+        sign=sign
+    )
+    sendp(pkg, count=5, inter=0.2, verbose=0)
+
+
+def check_sign(pub_key, sign, data):
+    if sign == 2707:
+        return True
+    else:
+        return False
+
+
+def receive_packet(team_id, pub_key, cmd):
+    def is_env(pkt):
+        if pkt.haslayer(Env):
+            if pkt.decoded == 31337 and pkt.team_id == team_id:
+                if check_sign(pub_key, pkt.sign, pkt.data):
+                    return True
+        return False
+
+    captured = {p.data for p in sniff(lfilter=is_env, timeout=3)}
+    for data in captured:
+        try:
+            data_bytes = int(data).to_bytes(300, byteorder='big').lstrip(b'\0')
+            if cmd in data_bytes:
+                return data_bytes.split(cmd)[1]
+        except:
+            pass
+    return None
 
 
 def gen_dh():
@@ -33,14 +88,6 @@ def shared_secret(p, a, B):
     return pow(B, a, p)
 
 
-def xor(s1, s2):
-    if not isinstance(s1, bytes):
-        s1 = s1.encode("utf8")
-    if not isinstance(s2, bytes):
-        s2 = s2.encode("utf8")
-    return b64encode(bytes(x ^ y for x, y in zip(s1, s2)))
-
-
 def close(code, public="", private=""):
     if public:
         print(public)
@@ -54,90 +101,67 @@ def check(*args):
     if not addr:
         close(INTERNAL_ERROR, None, "Check without ADDR")
 
-    for i in range(1, 3):
-        try:
-            sock = socket()
-            sock.connect((addr, PORT + i))
-            sock.send(b"check\n")
-            recv = sock.recv(3).strip()
-            try:
-                recv = int(recv)
-            except ValueError:
-                close(CORRUPT, "problem with %s" % SENSORS[i])
-        except net_error:
-            close(FAIL, "No connection to %s" % SENSORS[i])
-
 
 def put(*args):
     addr = args[0]
     flag_id = args[1]
     flag = args[2]
-    answer = ""
 
     if not addr or not flag_id or not flag:
         close(INTERNAL_ERROR, None, "Incorrect parameters")
+    #try:
+    p, g, A, a = gen_dh()
+    data = "start:%s:%s:%s" % (p, g, A)
+    team_id = int(addr.split(".")[2])
+    send_package(team_id, data)
+    # TODO: get pub_key for client
+    pub_key = 1
+
+    B = receive_packet(team_id, pub_key, b'pub:')
+    if not B:
+        close(CORRUPT, "DH negotiation failed",
+              "DH: not receive pub key (B)")
+
+    secret = 0
     try:
+        secret = shared_secret(p, a, int(B))
+    except:
+        close(CORRUPT, "DH negotiation wrong",
+              "DH: received wrong pub key (B)")
 
-        p, g, A, a = gen_dh()
-        data = int.from_bytes("start:%s:%s:%s" % (p, g, A), byteorder='big')
-        client_id = addr
-        # TODO: sign data
-        # TODO: get pub_key for client
-        # TODO: send wi-fi signed_data to client
-        # TODO: listen wi-fi to answer and check client_sign
-        # TODO: if packet.captured: B = packet.data
-        B = int()
-        secret = shared_secret(p, a, B)
-        data = int.from_bytes("put:%s" % flag, byteorder='big') * secret
+    data = "put:%s" % flag
+    send_package(team_id, data, secret)
+    answer = receive_packet(team_id, pub_key, b'ACCEPT:')
 
-        # 36 bytes
+    if answer:
+        close(OK, flag_id, None)
+    else:
+        close(CORRUPT, "ID not found",
+              "ID not found in ACCEPT")
 
-        # TODO: sign data
-        # TODO: send wi-fi signed_data to client
-        # TODO: listen wi-fi to answer and check client_sign
-        # TODO: if packet.captured:
-        # packed.data = int(packed.data).to_bytes(50, byteorder='big').lstrip(b'\0')
-        packet_data = b""
-        if b"ACCEPT:" in packet_data:
-            flag_id = packet_data.split(b"ACCEPT:")[1]
-            if flag_id:
-                close(OK, flag_id, None)
-            else:
-                close(CORRUPT, "ID not found",
-                      "ID not found in %s" % packet_data)
-        else:
-            close(CORRUPT, "Data not accepted by environ",
-                  "Not accepted: %s" % packet_data)
-        # TODO: else (packet not captured or timeout)
-        # close(CORRUPT, "No answer",
-        #       "Service did not respond to put")
-
-    except net_error:
-        close(FAIL, "No connection to %s" % addr)
-    except (KeyError, IndexError):
-        close(CORRUPT, "JSON structure", "Bad answer in %s" % answer)
+    #except:
+    #    close(FAIL, "No interface")
 
 
 def get(*args):
     addr = args[0]
-    checker_flag_id = args[1]
+    flag_id = args[1]
     flag = args[2]
-    vuln = int(args[3]) if len(args) > 3 else 1
-    answer = ""
-    if not addr or not checker_flag_id or not flag:
+    if not addr or not flag_id or not flag:
         close(INTERNAL_ERROR, None, "Incorrect parameters")
     try:
-        pass
-    except net_error:
+        answer = requests.get("http://%s:%s/%s" % (addr, PORT, flag_id))
+        if answer.status_code != 200:
+            close(GET_ERROR, private="Flag not found")
+        if answer.text != flag:
+            close(GET_ERROR, private="Bad flag by flag_id")
+        close(OK)
+    except:
         close(FAIL, "No connection to %s" % addr)
-    except (KeyError, IndexError):
-        close(CORRUPT, "JSON structure", "Bad answer in %s" % answer)
-    except ValueError:
-        close(GET_ERROR, private="Incorrect vuln")
 
 
 def info(*args):
-    close(OK, "vulns: 3:1")
+    close(OK, "vulns: 1")
 
 
 COMMANDS = {'check': check, 'put': put, 'get': get, 'info': info}
