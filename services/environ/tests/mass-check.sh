de0e82192c638111fb352ec48dc3cdbf3d6e8b0a
m-messiah <m.muzafarov@gmail.com>
2016-04-10 14:06:58 +0300

    🏡 environ: mass-check update

--- services/environ/tests/mass-check.sh
+++ services/environ/tests/mass-check.sh
@@ -3,23 +3,25 @@
 export WIFICARD=wlan0
 errors=0
 counter=0
+
 for count in {1..10}; do
-for i in {101..122}; do
-    echo -ne "team$i\t"
-    flag=$(echo flag$i | md5sum | cut -d ' ' -f 1)
-    ((counter++))
-    A=$(python3.5 environ.checker.py put 172.16.16.$i user$i $flag)
-    if [ $? -eq 101 ]; then
-        python3.5 environ.checker.py get 172.16.16.$i $A $flag
+    echo "Iter: $count. Errors: $errors"
+    for i in {101..122}; do
+        echo -ne "team$i\t"
+        flag=$(echo flag$i | md5sum | cut -d ' ' -f 1)
+        ((counter++))
+        A=$(python3.5 environ.checker.py put 172.16.16.$i user$i $flag)
         if [ $? -eq 101 ]; then
-        echo "OK"
+            python3.5 environ.checker.py get 172.16.16.$i $A $flag
+            if [ $? -eq 101 ]; then
+            echo "OK"
+            else
+            ((errors++))
+            fi
         else
         ((errors++))
         fi
-    else
-    ((errors++))
-    fi
-done
+    done
 done
 
-echo "Flags: $errors / $counter"
+echo "Errors: $errors / $counter"
