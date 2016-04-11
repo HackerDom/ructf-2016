e444ae5b02166ea279a19e90682196d621e2d9d7
Nikolay Zhuravlev <znick@znick.ru>
2016-04-12 01:40:32 +0500

    Move socks to http. Service only.

--- services/socks/src/main.rs
+++ services/socks/src/main.rs
@@ -1,113 +1,64 @@
-use std::io::Write;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::thread;
-use std::io::BufReader;
-use std::io::BufRead;
+extern crate hyper;
+extern crate urlparse;
+
 use std::sync::{Arc, Mutex};
 
+use std::io::Read;
+
 use std::collections::HashMap;
+
+use hyper::server::{Handler, Server, Request, Response};
+use hyper::status::StatusCode;
+
+use urlparse::urlparse;
+use urlparse::GetQuery;  // Trait
 
 
 struct Context {
     stor : Mutex<HashMap<String, String>>,
-    handlers : HashMap<String, fn(Vec<&str>, Arc<Context>) -> Result<String, String> >
 }
 
 impl Context {
     pub fn new() -> Context {
-        let mut context = Context{stor : Mutex::new(HashMap::new()), handlers: HashMap::new()};
-        context.handlers.insert("GET".to_string(), get);
-        context.handlers.insert("PUT".to_string(), put);
+        let mut context = Context{stor : Mutex::new(HashMap::new())};
         context
     }
 }
 
-fn get(args: Vec<&str>, context: Arc<Context>) -> Result<String, String> {
-    println!("GET!");
-    if args.len() != 2 {
-        return Err("Not enougth args!".to_string());
-    }
+impl Handler for Context {
+    fn handle(&self, mut req: Request, mut res: Response) {
+        // self.sender.lock().unwrap().send("start").unwrap();
+        // println!("{}", req.uri);
+        let url = urlparse(req.uri.to_string());
+        let query = url.get_parsed_query().unwrap();
 
-    let key = args[1];
-    let stor = context.stor.lock().unwrap();
-    match stor.get(key) {
-        Some(value) => {return Ok((value.clone()))},
-        None => return Err(("NOT FOUND".to_string())),
-    }
+        println!("{}", url.path);
 
-    Ok((String::new()))
-}
+        if url.path == "/search" {
+            let text = query.get_first_from_str("text").unwrap();
+            let stor = self.stor.lock().unwrap();
+            // let status: &mut StatusCode = res.status_mut();
+            let mut data: String;
 
-fn put(args: Vec<&str>, context: Arc<Context>) -> Result<String, String> {
-    println!("PUT!");
-    if args.len() != 3 {
-        return Err("Not enougth args!".to_string());
-    }
-
-    let key = args[1].to_string();
-    let value = args[2].to_string();
-    let mut stor = context.stor.lock().unwrap();
-    stor.insert(key, value.clone());
-
-    Ok((value))
-}
-
-
-fn handle_client(mut stream: TcpStream, context: Arc<Context>) {
-    let mut reader = BufReader::new(stream.try_clone().unwrap());
-    let mut buf = String::new();
-    loop {
-        // let context = context.clone();
-        buf.clear();
-        match reader.read_line(&mut buf) {
-            Ok(v) => {
-                let line = buf.trim();
-                // stream.write(line.as_bytes());
-                println!("Line: {:?}", line);
-                // if line.len() == 0 {
-                //     break;
-                // }
-
-                let mut args: Vec<&str> = line.split("\t").collect();
-                let handler_name = args[0];
-                println!("Handler name: {:?}", handler_name);
-                match context.handlers.get(&handler_name.to_string()) {
-                    Some(handler) => {
-                        match handler(args, context.clone()) {
-                            Ok(reply) => {
-                                stream.write(b"[OK] ");
-                                stream.write(reply.as_bytes());
-                                stream.write(b"\n");
-                            },
-                            Err(e) => {
-                                stream.write(b"[ERR] ");
-                                stream.write(e.as_bytes());
-                                stream.write(b"\n");
-                            }
-                        };
-                    },
-                    None => { stream.write(b"Unkwon command!\n"); }
-                }
-            },
-            Err(e) => {
-                break;
+            match stor.get(text.as_str()) {
+                Some(value) => { res.send(value.as_bytes()).unwrap() },
+                None => *res.status_mut() = StatusCode::NotFound
             }
+        }
 
+        if url.path == "/set" {
+            let mut stor = self.stor.lock().unwrap();
+            let mut data = String::new();
+            let text = query.get_first_from_str("text").unwrap();
+            req.read_to_string(&mut data);
+            stor.insert(text, data.clone());
         }
     }
-    // Ok(())
+
 }
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:9123").unwrap();
-    println!("listening started, ready to accept");
-    let mut context = Arc::new(Context::new());
-    for stream in listener.incoming() {
-        let context = context.clone();
-        thread::spawn(move || {
-            let mut stream = stream.unwrap();
-            handle_client(stream, context);
-        });
-    }
+    let mut context = Context::new();
+    Server::http("127.0.0.1:3000").unwrap()
+        .handle(context).unwrap();
 }
