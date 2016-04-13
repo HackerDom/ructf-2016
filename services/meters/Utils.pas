df248171cbaf72572c31e6945561f7dda6216b67
Tinkturianec <zaynullin-bulat@yandex.ru>
2016-04-13 18:16:26 +0500

    meters: try repear vuln

--- services/meters/Utils.pas
+++ services/meters/Utils.pas
@@ -17,8 +17,6 @@ interface
 	function GetSubTemplate(const module, name: string): string;
 	function StrToQWord(const s: string): QWord;
 	function htmlEncode(const str: string): string;
-	function ToPrettyString(const dt: TDateTime): string;
-
 
 implementation
 	uses
@@ -118,12 +116,6 @@ implementation
 	begin
 		result := StringReplace(str, '&', '&amp;', [rfReplaceAll]);
 		result := StringReplace(result, '<', '&lt;', [rfReplaceAll]);
-	end;
-
-	function ToPrettyString(const dt: TDateTime): string;
-	begin
-
-		result := 'set ' + IntToStr(-trunc(dt)) + ' days before at ' + FormatDateTime('hh:nn:ss', dt);
 	end;
 
 initialization
