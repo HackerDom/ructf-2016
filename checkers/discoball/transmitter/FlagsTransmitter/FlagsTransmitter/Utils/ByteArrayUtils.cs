53c6d0e08f1a7a1eeb67febfae32ad89162804bd
Konstantin Plotnikov <kost@hackerdom.ru>
2016-04-15 05:35:53 +0500

    FlagsTransmitter for DiscoBall service

---
+++ checkers/discoball/transmitter/FlagsTransmitter/FlagsTransmitter/Utils/ByteArrayUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Electro.Utils
+{
+	static class ByteArrayUtils
+	{
+		public static string ToHex(this byte[] bytes)
+		{
+			return bytes.ToHex(0, bytes.Length);
+		}
+
+		public static string ToHex(this byte[] bytes, int offset, int count)
+		{
+			if(bytes.Length < offset + count)
+				throw new ArgumentOutOfRangeException();
+			var array = new char[count << 1];
+			for(int i = 0; i < count; i++)
+			{
+				var b = bytes[offset + i];
+				var hb = b >> 4;
+				array[i << 1] = (char)(hb > 9 ? hb + ByteA - 10 : hb + Byte0);
+				var lb = b & 0xf;
+				array[(i << 1) + 1] = (char)(lb > 9 ? lb + ByteA - 10 : lb + Byte0);
+			}
+			return new string(array);
+		}
+
+		private const int Byte0 = '0';
+		private const int ByteA = 'a';
+	}
+}
---
+++ checkers/discoball/transmitter/FlagsTransmitter/FlagsTransmitter/Utils/CryptUtils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electro.Utils
+{
+	class CryptUtils
+	{
+		public static string CalcHash(string str)
+		{
+			using(var sha1 = SHA1.Create())
+			{
+				var buff = Encoding.UTF8.GetBytes(str);
+				return sha1.TransformFinalBlock(buff, 0, buff.Length).ToHex();
+			}
+		}
+	}
+}
